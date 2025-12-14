731c48d0c2909987fd053d6ae8dbaa401c92498d
Alvaro <afisicos@gmail.com>
2025-12-14 13:47:01 +0100

    vercel

--- mapgenerator/src/MapEditor.tsx
+++ mapgenerator/src/MapEditor.tsx
@@ -42,7 +42,6 @@ export default function MapEditor() {
     setExportTogether,
     floorWithVolume,
     setFloorWithVolume,
-    hasClosedPolygons,
     setSelectedPolygonId,
     setSelectedPointIndex,
     setSelectedPoints,
